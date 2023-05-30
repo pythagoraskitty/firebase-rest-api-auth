d9b37d7f7d9a5456eb875dbc92d1c86b643c5b3c
Camillia Smith Barnes <csb@alumni.harvard.edu>
2023-05-30 00:06:09 -0400

    Update __init__.py

--- firebase/__init__.py
+++ firebase/__init__.py
@@ -5,15 +5,12 @@
 # --------------------------------------------------------------------------------------
 
 
-"""A simple python wrapper for Google's `Firebase`_ REST APIs.
+"""A simple python wrapper for Google's `Firebase`_ REST API Auth.
 
 .. _Firebase: https://firebase.google.com/
 """
 
 from .auth import Auth
-from .storage import Storage
-from .database import Database
-from .firestore import Firestore
 from ._custom_requests import _custom_request
 from ._service_account_credentials import _service_account_creds_from_secret
 
@@ -68,32 +65,3 @@ class Firebase:
 		"""
 
 		return Auth(self.api_key, self.credentials, self.requests, client_secret=client_secret)
-
-	def database(self):
-		"""Initializes and returns a new Firebase Realtime Database
-		instance.
-
-		:return: A newly initialized instance of Database.
-		:rtype: Database
-		"""
-
-		return Database(self.credentials, self.database_url, self.requests)
-
-	def firestore(self):
-		"""Initializes and returns a new Firebase Cloud Firestore
-		instance.
-
-		:return: A newly initialized instance of Firestore.
-		:rtype: Firestore
-		"""
-
-		return Firestore(self.api_key, self.credentials, self.project_id, self.requests)
-
-	def storage(self):
-		"""Initializes and returns a new Firebase Storage instance.
-
-		:return: A newly initialized instance of Storage.
-		:rtype: Storage
-		"""
-
-		return Storage(self.credentials, self.requests, self.storage_bucket)
